b310a6ed9e96855e2424c6620abe34f88181a369
Mimi Kim <mimikimwebdeveloper@gmail.com>
2019-12-10 23:41:57 -0500

    updating github link in footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
   <footer className="footer text-center">
     Mimi Kim, 2019
     <br />
-    <a href="https://github.com/mimikim/" rel="noopener noreferrer"  target="_blank">See Source on Github</a>
+    <a href="https://github.com/mimikim/JEOPARDY-React-App/" rel="noopener noreferrer"  target="_blank">See Source on Github</a>
   </footer>
 );
 
